9a8d365cc358485e7cd330364d5d5da638029962
R3n0 Mac-2014 <renomartisan@gmail.com>
2016-12-18 16:03:15 -0500

    se cambió un poco el código

--- test.cpp
+++ test.cpp
@@ -6,10 +6,10 @@ int main (){
 	for (int i = 0; i < 40; i++ ){
 		for (int j = 0; j < 80; j++){
 			float r1 = rand() / (float) RAND_MAX;
-			float r2 = rand() / (float) RAND_MAX;
-			r1 *= 2;
-			r1 = 30 + r1 + j;
-			if(r2 > (i / 40.0) || -i < -j){
+			float r2 = rand() / (float) RAND_MAX; 
+			r1 *= 5;
+			r1 += (30 + j);
+			if(r2 > (i / 40.0) || i > j || j > (80 - i)){
 				r1 = ' ';
 			}
 			cout << (char) r1;
@@ -17,5 +17,4 @@ int main (){
 		cout << endl;
 	}
 	return 0;
-
 }
